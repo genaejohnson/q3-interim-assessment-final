4640c076df14a21f028a3c3140e0dfe7ac1fc046
Genae Johnson <gsjohnson@wlapcs.org>
2017-04-11 17:29:28 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup(){
+    createCanvas(600,400);
+    
+}
+
+function draw(){
+    background(50,50,50);
+   rect(275,175,50,50);
+   ellipse(300,200,50,50)
+   fill(7,157,249);
+   
+}
+
+
+function ball(){
+    
+}
+
+
